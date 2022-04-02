fbdd10f3cc15c942f6906206007d1fa6623b88e2
root <root@josiemerson.ntb>
2022-04-02 15:54:11 -0300

    data and config

---
+++ config/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+$g_hostname               = 'mysql';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'mantisbt';
+$g_db_password            = 'mantisbt';
+
+$g_db_table_prefix          = '';
+$g_db_table_plugin_prefix   = '';
+$g_db_table_suffix          = '';
+
+$g_default_timezone       = 'America/Sao_Paulo';
+
+$g_crypto_master_salt     = 'YDrBIr/lXlzZyaZT/63uNmSsXKC8rPKTo/tSZuLXcUM=';
